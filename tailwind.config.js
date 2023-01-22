e899183c9089b091f26ebad9419d65f3629fc4d0
saiapowainur <saiapow.ainur@gmail.com>
2023-01-22 21:08:39 +0400

    added dark preferences

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const prelinePlugin = require('preline/plugin')
 
 /** @type {import('tailwindcss/types').Config} */
 const config = {
+	darkMode: 'media',
 	content: ['build/preline/dist/*.js', 'index.html'],
 	theme: {
 	},
